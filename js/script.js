d16865a90386dfafaf7c3387915108919496e262
unknown <noubiahance@gmail.com>
2024-07-26 14:21:34 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('popup').addEventListener('click', function (e) {
+    document.getElementById('text').innerHTML = ("orem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur dolores molestias nemo beatae eum doloremque minima saepe aliquam cupiditate voluptatum mod")
+ })
